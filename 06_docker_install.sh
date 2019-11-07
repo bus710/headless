77f4baa11d63171bb52d57eb73c6c7b3dd87b5b7
bus710 <bus710@gmail.com>
2019-11-07 08:53:42 -0800

    A minor update

--- 06_docker_install.sh
+++ 06_docker_install.sh
@@ -32,10 +32,6 @@ echo
 
 if [ $ans == "y" ]
 then 
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-    add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" 
-
-    apt remove docker-ce docker-ce-cli containerd.io
     apt update
     apt install -y docker-ce docker-ce-cli containerd.io
 
