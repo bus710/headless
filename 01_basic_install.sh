61d3b0ea5c525c436ee22e43c7bb94e2d282ae1e
bus710 <bus710@gmail.com>
2019-11-14 09:24:17 -0800

    A minor update

--- 01_basic_install.sh
+++ 01_basic_install.sh
@@ -25,33 +25,34 @@ then
     apt update
 
     # For general packages
-    apt install -y build-essential 
-    apt install -y cmake 
-    apt install -y exuberant-ctags
-    apt install -y git
-    apt install -y bash-completion 
-    apt install -y command-not-found 
-    apt install -y bmon 
-    apt install -y htop
-    apt install -y tmux
-    apt install -y minicom
-    apt install -y powerline
-    apt install -y curl
-    apt install -y tree
+    apt install -y git \
+        bmon \
+        htop \
+        tmux \
+        curl \
+        tree \
+        cmake \
+        minicom \
+        neofetch \
+        powerline \
+        build-essential \
+        exuberant-ctags \
+        bash-completion \
+        command-not-found
 
     # For Flutter SDK 
     apt install -y lib32stdc++6
 
     # For daemons required
-    apt install -y openssh-server
-    apt install -y avahi-daemon
-    apt install -y avahi-utils
+    apt install -y openssh-server \
+        avahi-daemon \
+        avahi-utils
 
     # For Docker
-    apt install -y apt-transport-https
-    apt install -y ca-certificates
-    apt install -y gnupg-agent 
-    apt install -y software-properties-common
+    apt install -y gnupg-agent \
+        apt-transport-https \
+        ca-certificates \
+        software-properties-common
 
     # To apply Go/Flutter SDK path to the PATH variable
     cat bashrc >> /home/$LOGNAME/.bashrc
