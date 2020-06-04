db054c0dce49f696624982f4395511923eb05e70
bus710 <bus710@gmail.com>
2020-06-04 11:13:16 -0700

    A minor update

--- 00_git_setup.sh
+++ 00_git_setup.sh
@@ -1,4 +1,11 @@
+#!/bin/bash
+
 git config remote.origin.url git@github.com:bus710/headless.git
+
 git config user.email "bus710@gmail.com"
 git config user.name "bus710"
 git config push.default simple
+
+git config --global user.email "bus710@gmail.com"
+git config --global user.name "bus710"
+git config --global push.default simple
