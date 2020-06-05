38c07cc4a7abc0aff6e0668b43d94e9d412cba8a
bus710 <bus710@gmail.com>
2020-06-04 18:18:40 -0700

    A minor update

--- 99_git_push.sh
+++ 99_git_push.sh
@@ -1,15 +1,21 @@
 # Pushing commits to a pre-configured repository.
 # Can check the repo from 00_git_setup.sh.
 
-echo
+echo -e "\e[91m"
 echo "git diff --name-status"
-echo
+echo -e "\e[39m"
 
 git diff --name-status
 
-echo
+echo -e "\e[91m"
+echo "git ls-files --others --exclude-standard"
+echo -e "\e[39m"
+
+git ls-files --others --exclude-standard
+
+echo -e "\e[91m"
 echo "Do you want to add and commit all changes? (y/n)"
-echo 
+echo -e "\e[39m"
 
 read -n 1 ans
 
