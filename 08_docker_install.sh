f5f479cb47c55659cafceedef8bad338a354d4c0
bus710 <bus710@gmail.com>
2020-06-05 17:47:17 -0700

    wip

--- 08_docker_install.sh
+++ 08_docker_install.sh
@@ -1,30 +1,16 @@
 #!/bin/bash
 
-DOCKER_COMPOSE_VERSION="1.24.0"
-
 if [ "$EUID" != 0 ]
 then 
     echo "Please run as the super user (w/ sudo)"
     exit
 fi
 
-OSNAME="$(lsb_release -ds)"
-
-if [[ "$OSNAME" == *"Debian"* ]]
-then
-    echo "Sorry, Debian is not supported by this script"
-    exit
-fi
-
 echo
 echo -e "\e[91m"
-echo "Please check the website if there is a newer version of docker-compose"
-echo "https://github.com/docker/compose/releases"
-echo
-echo "$DOCKER_COMPOSE_VERSION will be installed"
-echo -e "\e[39m"
-echo
+echo "Docker and docker-compose will be installed"
 echo "Do you want to install? (y/n)"
+echo -e "\e[39m"
 echo
 
 read -n 1 ans
@@ -34,18 +20,14 @@ if [ $ans == "y" ]
 then 
     apt update
     apt install -y docker.io
+    apt install -y docker-compose
 
     systemctl enable docker
     systemctl start docker
 
-    # For Docker-compose
-    rm -rf /usr/local/bin/docker-compose
-    curl -L "https://github.com/docker/compose/releases/download/$DOCKER_COMPOSE_VERSION/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-    chmod +x /usr/local/bin/docker-compose
-
     echo
     echo "!! Docker daemon is enabled/started !!"
-    echo "!! Check Docker-Compose version (Currently $DOCKER_COMPOSE_VERSION) !!"
+    echo 
     echo "!! Add the user to the docker group !!"
     echo "!! sudo usermod -aG docker '${LOGNAME}' !!"
     echo
