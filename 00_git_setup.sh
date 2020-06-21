82dab939c18d4880a6099621f7e122b772c13ff1
bus710 <bus710@gmail.com>
2020-06-21 13:02:40 -0700

    wip

--- 00_git_setup.sh
+++ 00_git_setup.sh
@@ -11,6 +11,9 @@ git config --global user.name "bus710"
 git config --global push.default simple
 git config --global core.editor nvim
 
+# this turns off pager config that works like the less command
+git config --global pager.branch false 
+
 echo 
 echo "Git config -l"
 echo 
