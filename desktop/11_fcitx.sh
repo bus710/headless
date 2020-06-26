d504ed48c3d38a64047ead1d0ed92dcc986f4371
bus710 <bus710@gmail.com>
2020-06-26 11:10:36 -0700

    wip

--- desktop/11_fcitx.sh
+++ desktop/11_fcitx.sh
@@ -3,8 +3,13 @@
 sudo apt install fcitx fcitx-config-gtk fcitx-hangul
 im-config -n fcitx
 
+sudo bash -c 'echo "GTK_IM_MODULE=fcitx" >> /etc/environment'
+sudo bash -c 'echo "QT_IM_MODULE=fcitx" >> /etc/environment'
+sudo bash -c 'echo "XMODIFIERS=@im=fcitx" >> /etc/environment'
+
 echo
 echo "Done!"
-echo "	- reboot and run fcitx-config-gtk3"
-echo "	- also, install the korean support in settings - language/method"
+echo "- add /usr/bin/fcitx to Startup Applications"
+echo "- reboot and run fcitx-config-gtk3"
+echo "- also, install the korean support in settings - language/method"
 echo
