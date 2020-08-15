b528908668758cab1137727d35d17ad7611dc35c
bus710 <bus710@gmail.com>
2020-08-14 21:01:02 -0700

    wip

--- desktop/01_home_cleanup.sh
+++ desktop/01_home_cleanup.sh
@@ -11,11 +11,11 @@ echo
 echo "Home directory cleanup."
 echo
 
-rmdir ~/Documents
-rmdir ~/Music
-rmdir ~/Videos
-rmdir ~/Templates
-rmdir ~/Public
+rm -rf ~/Documents
+rm -rf ~/Music
+rm -rf ~/Videos
+rm -rf ~/Templates
+rm -rf ~/Public
 
 echo
 echo "Done"
