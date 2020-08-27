53ce7345c5b1d2aee6fb102412a2de131e426d4b
bus710 <bus710@gmail.com>
2020-08-26 20:31:26 -0700

    wip

--- desktop/03_heavy_packages.sh
+++ desktop/03_heavy_packages.sh
@@ -33,6 +33,7 @@ wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-ke
 sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 sudo apt update 
 sudo apt install -y google-chrome-stable
+sudo rm -rf /etc/apt/sources.list.d/google-chrome.list
 
 echo
 echo "And more:"
