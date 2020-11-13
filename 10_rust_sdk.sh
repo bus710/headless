831bd9cc5cfe0b6adda51525e7d78bb34f185044
bus710 <bus710@gmail.com>
2020-11-13 10:50:16 -0800

    wip

---
+++ 10_rust_sdk.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+source $HOME/.cargo/env
+
+echo
+echo "See the version below"
+echo
+
+rustc --version
+cargo --version
+
+echo
+echo Update rust and add components
+echo
+
+rustup update
+rustup component add rustfmt clippy rls rust-analysis rust-src
