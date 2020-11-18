00289011d3fa7e2df05632e3f969ffd40be3ca47
bus710 <bus710@gmail.com>
2020-11-18 08:04:10 -0800

    wip

--- 05_rust.sh
+++ 05_rust.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 # https://www.rust-lang.org/tools/install
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
@@ -18,6 +20,12 @@ echo
 
 rustup update
 rustup component add rustfmt clippy rls rust-analysis rust-src
+
+echo
+echo "Additional packages for OpenCV"
+echo
+
+sudo apt install -y cmake ninja-build clang libclang-dev
 
 echo
 echo "Done"
