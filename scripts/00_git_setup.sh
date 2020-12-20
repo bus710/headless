b2d3956ce1b49cfee27f946b6829d8bd21fd26ee
bus710 <bus710@gmail.com>
2020-12-20 15:09:27 -0800

    wip

--- scripts/00_git_setup.sh
+++ scripts/00_git_setup.sh
@@ -7,12 +7,14 @@ git config remote.origin.url git@github.com:bus710/headless.git
 git config user.email "bus710@gmail.com"
 git config user.name "bus710"
 git config push.default simple
+git config pull.rebase false
 
 git config --global user.email "bus710@gmail.com"
 git config --global user.name "bus710"
 git config --global push.default simple
 git config --global core.editor /home/${LOGNAME}/.tools/nvim
 git config --global core.pager cat
+git config --global pull.rebase false
 
 # this turns off pager config that works like the less command
 git config --global pager.branch false 
