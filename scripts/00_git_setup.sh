11ce37abe04acb82fff3eaeae7379ca2e741b25e
bus710 <bus710@gmail.com>
2021-01-28 14:15:07 -0800

    wip

--- scripts/00_git_setup.sh
+++ scripts/00_git_setup.sh
@@ -14,33 +14,20 @@ git config --global pager.branch false
 
 # to include the config files conditionally
 CONFIG=$(cat ~/.gitconfig)
-if [[ $CONFIG =~ "includeIf" ]]; then
-  echo "The config already has some includeIf section"
-else
-  echo "The config doesn't have any includeIf section"
-  echo '[includeIf "gitdir:~/repo/"]' >> ~/.gitconfig
-  echo "    path = ~/repo/.gitconfig" >> ~/.gitconfig
-  echo "" >> ~/.gitconfig
-  echo '[includeIf "gitdir:~/repo-work/"]' >> ~/.gitconfig
-  echo "    path = ~/repo-work/.gitconfig" >> ~/.gitconfig
-  echo "" >> ~/.gitconfig
-  echo '[url "ssh://git@git.egnyte-internal.com:"]'
-  echo "    insteadOf = https://git.egnyte-internal.com"
-  echo "" >> ~/.gitconfig
+git config --global includeif.gitdir:~/repo/.path ~/repo/.gitconfig   
+git config --global includeif.gitdir:~/repo-work/.path ~/repo-work/.gitconfig
+git config --global url."ssh://git@git.egnyte-internal.com:".insteadOf "https://git.egnyte-internal.com"
 
-  # create files
-  mkdir -p ~/repo
-  touch ~/repo/.gitconfig
-  echo "[user]" >> ~/repo/.gitconfig
-  echo "    name = bus710" >> ~/repo/.gitconfig
-  echo "    email = bus710@gmail.com" >> ~/repo/.gitconfig
+mkdir -p ~/repo
+touch ~/repo/.gitconfig
+mkdir -p ~/repo-work
+touch ~/repo-work/.gitconfig
 
-  mkdir -p ~/repo-work
-  touch ~/repo-work/.gitconfig
-  echo "[user]" >> ~/repo-work/.gitconfig
-  echo "    name = skim" >> ~/repo-work/.gitconfig
-  echo "    email = skim@git.egnyte-internal.com" >> ~/repo-work/.gitconfig
-fi
+git config --file=~/repo/.gitconfig --add user.name bus710
+git config --file=~/repo/.gitconfig --add user.email bus710@gmail.com
+
+git config --file=~/repo-work/.gitconfig --add user.name skim
+git config --file=~/repo-work/.gitconfig --add user.email skim@git.egnyte-internal.com
 
 # print
 
