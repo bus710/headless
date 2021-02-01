c9a81512cf2be105356fd09cc1eb5d06314176df
bus710 <bus710@gmail.com>
2021-02-01 09:52:31 -0800

    wip

--- 21_flutter_sdk.sh
+++ 21_flutter_sdk.sh
@@ -3,8 +3,7 @@
 set -e
 
 URL="https://storage.googleapis.com/flutter_infra/releases/beta/linux/"
-VERSION="flutter_linux_1.21.0-9.2.pre-beta.tar.xz"
-
+VERSION="flutter_linux_1.25.0-8.3.pre-beta.tar.xz"
 
 if [[ "$EUID" == 0 ]]
 then echo "Please run as a normal user (w/o sudo)"
