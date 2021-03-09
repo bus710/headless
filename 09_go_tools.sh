f99fea2579f0f15c3960ecf260040c7a79bdaf37
bus710 <bus710@gmail.com>
2021-03-09 09:08:06 -0800

    wip

--- 09_go_tools.sh
+++ 09_go_tools.sh
@@ -9,29 +9,20 @@ then echo "Please run as normal user (w/o sudo)"
   exit
 fi
 
-CPU_TYPE=$(uname -p)
+CPU_TYPE=$(uname -m)
 
 if [[ "$CPU_TYPE" == "x86_64" ]]; then
     echo "" # for future
 fi
 
 echo 
-echo "Install dlv"
+echo "Install dlv, goexec, wasmserve, swaggo"
 echo 
 
 go get -u github.com/go-delve/delve/cmd/dlv
-
-echo
-echo "Install goexec"
-echo
-
 go get -u github.com/shurcooL/goexec
-
-echo
-echo "Install wasmserve"
-echo
-
 go get -u github.com/hajimehoshi/wasmserve
+go get -u github.com/swaggo/swag/cmd/swag
 
 echo
 echo "Install vim-go plugins"
