2373ff6271166ed02ed309a602b8c78462b954cb
bus710 <bus710@gmail.com>
2023-01-30 21:30:51 -0800

    progress

--- 23_erlang.sh
+++ 23_erlang.sh
@@ -120,7 +120,7 @@ install_escript_symbol(){
 
     # To help Erlang_LS vscode extention.
     # In SwayWM, the "bindsym => exec" shortcut doesn't pass the PATH to VSCODE.
-    sudo ln -s /home/$LOGNAME/.asdf/shims/escript escript
+    sudo ln -s /home/$LOGNAME/.asdf/shims/escript /usr/local/bin/escript
 }
 
 check_installed_versions(){
