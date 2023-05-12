ed7fcfbc7f0550bf241800716efdcc3ea77aca7e
bus710 <bus710@gmail.com>
2023-05-11 17:42:39 -0700

    progress

--- desktop/lxqt/01_lxqt.sh
+++ desktop/lxqt/01_lxqt.sh
@@ -60,6 +60,49 @@ configure_lxqt(){
 
     rm -rf /home/$LOGNAME/Downloads
     mkdir /home/$LOGNAME/Downloads
+
+    term_color_red
+    echo "Configure LxQt shortcuts"
+    term_color_white
+
+    # Change global screen scaling to factor 1.25
+
+    # Change Sweep up/down for desktop switching to nothing
+
+    # Enable trackpad tap to click
+    # Enable trackpad natural scrolling
+    # Enable trackpad tap and drag
+
+    # Disable Idleness Watcher
+
+    # Open terminal: Meta+Return
+    # Open LxQt Configuration Center: Meta+g
+    # Open File browser: Meta+t
+    # Open Chrome: Meta+y
+    # Open Code: Meta+u
+    # Open Zathura: Meta+z
+    # Close the current window: Meta+Shift+q
+
+    # Switch to screen 1: Meta+1
+    # Switch to screen 2: Meta+2
+    # Switch to screen 3: Meta+3
+    # Switch to screen 4: Meta+4
+    # Switch to left desktop: Meta+i
+    # Switch to right desktop: Meta+o
+    # Move the current window to left desktop: Meta+Alt+i
+    # Move the current window to right desktop: Meta+Alt+o
+
+    # Lower screen brightness: Meta+Alt+1
+    # Higher screen brightness: Meta+Alt+2
+    # Lower volume: Meta+Alt+8
+    # Lower volume: Meta+Alt+9
+    # Mute: Meta+Alt+0
+    # Screen lock: Meta+Alt+l
+    # Power off: Meta+Alt+k
+
+    # Replace CapsLock to Ctrl
+    # Default Font for user interface => Point Size: 12
+    # Wallpaper image file:
 }
 
 post (){
