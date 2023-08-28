b7c8b3174e76ee0d0d431b42fc2a385508da8d6a
bus710 <bus710@gmail.com>
2023-08-28 13:09:18 -0700

    progress

--- 06_astro_nvim.sh
+++ 06_astro_nvim.sh
@@ -3,7 +3,8 @@
 set -e
 
 OS_TYPE=$(lsb_release -i)
-ARCH_TYPE=$(uname -m)
+ARCH=""
+ARCH2=""
 
 if [[ "$EUID" == 0 ]]; then
     echo "Please run as normal user (w/o sudo)"
@@ -20,7 +21,7 @@ term_color_white () {
 
 confirmation(){
     term_color_red
-    echo "This will delete nvim and your nvim config first"
+    echo "This will backup previous nvim configs and install AstroNvim config"
     echo "Do you want to install? (y/n)"
     echo
     term_color_white
@@ -32,164 +33,126 @@ confirmation(){
     if [[ ! $ans == "y" ]]; then
         exit -1
     fi
-}
 
-install_neovim(){
-    HOME="/home/$LOGNAME"
-
-    term_color_red
-    echo "Install neovim"
-    term_color_white
-
-    sudo apt remove -y \
-        neovim \
-        neovim-runtime
-
-    if [[ $OS_TYPE =~ "Ubuntu" ]]; then
-        term_color_red
-        echo "Add the neovim PPA for unstable"
-        term_color_white
-
-        sudo add-apt-repository ppa:neovim-ppa/unstable
-        sudo apt update
-        sudo apt install -y neovim
-
-    elif [[ $OS_TYPE =~ "Debian" && $ARCH_TYPE =~ "x86_64" ]]; then
-        term_color_red
-        echo "Download the nightly pre-built from Github"
-        term_color_white
-
-        mkdir -p /home/$LOGNAME/Downloads
-        cd /home/$LOGNAME/Downloads
-
-        #wget -q https://github.com/neovim/neovim/releases/download/v0.8.3/nvim-linux64.deb
-        #sudo dpkg -i nvim-linux64.deb
-        #rm -rf nvim-linux64.deb
-
-        wget -q \
-            https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage \
-            --output-document nvim
-        chmod +x nvim
-        sudo chown root:root nvim
-        sudo mv nvim /usr/bin
-
-        cd -
-    else
-        term_color_red
-        echo "Just install from the OS repo"
-        term_color_white
-
-        sudo apt install -y neovim
+    CPU_TYPE=$(uname -m)
+    if [[ $CPU_TYPE != "x86_64" && $CPU_TYPE != "aarch64" ]]; then
+        echo "x86_64 or aarch64 can be used."
+        exit
     fi
 
-    term_color_red
-    echo "Install required packages"
-    term_color_white
-
-    sudo apt install -y fuse libfuse2 ack-grep
-
-    term_color_red
-    echo "Clean up existing configuration"
-    term_color_white
-
-    rm -rf /home/$LOGNAME/.config/nvim/*
-    mkdir -p /home/$LOGNAME/.tools
-
-    term_color_red
-    echo "Create a symlink"
-    term_color_white
-
-    sudo rm -rf /usr/bin/nv
-    sudo ln -s /usr/bin/nvim /usr/bin/nv
-}
-
-install_dependencies(){
-    term_color_red
-    echo "Install nvim dependencies"
-    term_color_white
-
-    chown $LOGNAME:$LOGNAME /home/$LOGNAME/.cache -R
-
-    # Got warning that says - externally managed environment
-    # system wide python packages should be installed via apt
-    #pip3 install --user -U testresources
-    #pip3 install --user -U wheel
-    #pip3 install --user -U setuptools --no-warn-script-location
-    #pip3 install --user -U neovim
-    #pip3 install --user -U pynvim
-
-    sudo apt install -y \
-        python3-testresources \
-        python3-wheel \
-        python3-setuptools \
-        python3-neovim \
-        python3-pynvim
-    npm install -g neovim
-}
-
-update_configuration(){
-    term_color_red
-    echo "Copy the config files and run the post process"
-    term_color_white
-
-    mkdir -p /home/$LOGNAME/.config/nvim
-    mkdir -p /home/$LOGNAME/.config/nvim/autoload
-    mkdir -p /home/$LOGNAME/.config/nvim/plugged
-
-    mkdir -p /home/$LOGNAME/.local
-    mkdir -p /home/$LOGNAME/.tools
-
-    curl -fLo /home/$LOGNAME/.config/nvim/autoload/plug.vim \
-        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-    cp init.vim /home/$LOGNAME/.config/nvim/init.vim
-    cp ./coc-settings.json /home/$LOGNAME/.config/nvim/coc-settings.json
-
-    chown -R $LOGNAME:$LOGNAME /home/$LOGNAME/.config
-    chown -R $LOGNAME:$LOGNAME /home/$LOGNAME/.local
-    chown -R $LOGNAME:$LOGNAME /home/$LOGNAME/.tools
-}
-
-check_version(){
-    term_color_red
-    echo "Neovim version"
-    term_color_white
-
-    nvim -v
-}
-
-configure_runcom(){
-    term_color_red
-    echo "Configure runcom"
-    term_color_white
-
-    if [[ -f /usr/bin/nvim ]]; then
-        sed -i '/#NVIM_0/c\export PATH=\$PATH:$HOME/.tools' /home/$LOGNAME/.shrc
+    if [[ $CPU_TYPE == "x86_64" ]]; then
+        ARCH="x64"
+        ARCH2="amd64"
+    fi
+    if [[ $CPU_TYPE == "aarch64" ]]; then
+        ARCH="arm64"
+        ARCH2="aarch64"
     fi
 }
 
-install_plugins(){
+install_tree_sitter(){
     term_color_red
-    echo "Install plugins"
+    echo "Install tree-sitter"
     term_color_white
 
-    nvim -c :PlugInstall
+    cd /home/$LOGNAME/Downloads
+    rm -rf tree-sitter*
+    sudo rm -rf /usr/local/bin/tree-sitter
+
+    TREE_SITTER_VERSION=$(curl -o- -s https://api.github.com/repos/tree-sitter/tree-sitter/releases/latest | jq -r '.tag_name')
+    echo "$TREE_SITTER_VERSION"
+
+    wget -O tree-sitter.gz \
+        https://github.com/tree-sitter/tree-sitter/releases/download/${TREE_SITTER_VERSION}/tree-sitter-linux-${ARCH}.gz
+
+    gzip -d tree-sitter.gz
+    chmod 550 tree-sitter
+    sudo mv tree-sitter /usr/local/bin
+
+    cd -
+}
+
+install_nerd_fonts(){
+    term_color_red
+    echo "Install nerd-fonts"
+    term_color_white
+
+    cd /home/$LOGNAME/repo
+
+    if [[ -d nerd-fonts ]]; then
+	echo
+        echo "A nerd-fonts directory exists"
+	echo
+        return
+    fi
+
+    term_color_red
+    echo "Cloning might take some time"
+    term_color_white
+
+    git clone --depth 1 https://github.com/ryanoasis/nerd-fonts.git
+
+    cd nerd-fonts
+    ./install.sh
+
+    cd -
+}
+
+install_ripgrep(){
+    term_color_red
+    echo "Install ripgrep"
+    term_color_white
+
+    sudo apt install -y ripgrep
+}
+
+install_lazy_git(){
+    term_color_red
+    echo "Install laygit"
+    term_color_white
+
+    LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+    curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+    tar xf lazygit.tar.gz lazygit
+    sudo install lazygit /usr/local/bin
+}
+
+backup_previous_configuration(){
+    term_color_red
+    echo "Backup previous configuration"
+    term_color_white
+
+    rm -rf /home/$LOGNAME/.config/nvim.bak/nvim
+
+    if [[-d /home/$LOGNAME/.config/nvim ]]; then
+        mv /home/$LOGNAME/.config/nvim /home/$LOGNAME/.config/nvim.bak￼
+        mv /home/$LOGNAME/.local/share/nvim /home/$LOGNAME/.local/share/nvim.bak
+        mv /home/$LOGNAME/.local/state/nvim /home/$LOGNAME/.local/state/nvim.bak
+        mv /home/$LOGNAME/.cache/nvim /home/$LOGNAME/.cache/nvim.bak
+    fi
+}
+
+
+install_astro_nvim(){
+    term_color_red
+    echo "Install AstroNvim"
+    term_color_white
+
+    git clone --depth 1 https://github.com/AstroNvim/AstroNvim ~/.config/nvim
 }
 
 post(){
     echo
     echo "Done"
     echo
-
-    # Do this in case of error - no python3 provider found
-    # apt install python3-pynvim
 }
 
 trap term_color_white EXIT
 confirmation
-install_neovim
-install_dependencies
-update_configuration
-check_version
-configure_runcom
-install_plugins
+install_tree_sitter
+install_nerd_fonts
+install_ripgrep
+install_lazy_git
+backup_previous_configuration
+install_astro_nvim
 post
