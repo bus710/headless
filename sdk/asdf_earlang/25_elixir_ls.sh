6235562e7c7adbec0d5e64d62e573dcea9c8400d
bus710 <bus710@gmail.com>
2023-08-30 13:26:37 -0700

    progress

--- sdk/asdf_earlang/25_elixir_ls.sh
+++ sdk/asdf_earlang/25_elixir_ls.sh
@@ -2,60 +2,55 @@
 
 set -e
 
-if [[ "$EUID" == 0 ]]
-then echo "Please run as normal user (w/o sudo)"
-    exit
+if [[ "$EUID" == 0 ]]; then
+	echo "Please run as normal user (w/o sudo)"
+	exit
 fi
 
-term_color_red () {
-    echo -e "\e[91m"
+term_color_red() {
+	echo -e "\e[91m"
 }
 
-term_color_white () {
-    echo -e "\e[39m"
+term_color_white() {
+	echo -e "\e[39m"
 }
 
 install_language_server() {
-    term_color_red
-    echo "install language server for nvim"
-    term_color_white
+	term_color_red
+	echo "install language server for nvim"
+	term_color_white
 
-    
-    term_color_red
-    echo "install elixir-ls for nvim coc"
-    term_color_white
+	# term_color_red
+	# echo "install elixir-ls for nvim coc"
+	# term_color_white
+	#
+	# cd ~/Downloads
+	# LS_VERSION=$(curl -o- -s https://api.github.com/repos/elixir-lsp/elixir-ls/releases/latest | jq -r '.tag_name')
+	# wget https://github.com/elixir-lsp/elixir-ls/releases/download/${LS_VERSION}/elixir-ls-${LS_VERSION}.zip \
+	#     -O elixir-ls.zip
+	# unzip elixir-ls.zip -d /home/$LOGNAME/.config/nvim/plugged/coc-elixir/els-release
+	# rm -rf /home/$LOGNAME/Downloads/elixir-ls.zip
 
-    #rm -rf /home/$LOGNAME/.config/nvim/plugged/coc-elixir/els-release
-    cd ~/Downloads
-    LS_VERSION=$(curl -o- -s https://api.github.com/repos/elixir-lsp/elixir-ls/releases/latest | jq -r '.tag_name')
-    wget https://github.com/elixir-lsp/elixir-ls/releases/download/${LS_VERSION}/elixir-ls-${LS_VERSION}.zip \
-        -O elixir-ls.zip
+	term_color_red
+	echo "install and build elixir-ls"
+	term_color_white
 
-    unzip elixir-ls.zip -d /home/$LOGNAME/.config/nvim/plugged/coc-elixir/els-release
+	rm -rf /home/$LOGNAME/.elixir-ls
+	git clone https://github.com/elixir-lsp/elixir-ls.git /home/$LOGNAME/.elixir-ls
+	cd /home/$LOGNAME/.elixir-ls
+	mix deps.get && mix compile && mix elixir_ls.release -o release
 
-    rm -rf /home/$LOGNAME/Downloads/elixir-ls.zip
-
-
-    term_color_red
-    echo "install and build elixir-ls"
-    term_color_white
-
-    rm -rf /home/$LOGNAME/.elixir-ls
-    git clone https://github.com/elixir-lsp/elixir-ls.git /home/$LOGNAME/.elixir-ls
-    cd /home/$LOGNAME/.elixir-ls
-    mix deps.get && mix compile && mix elixir_ls.release -o release
-
-    # term_color_red
-    # echo "activate coc-elixir"
-    # term_color_white
-    #
-    # nvim -c "CocInstall coc-elixir"
+	# term_color_red
+	# echo "activate coc-elixir"
+	# term_color_white
+	#
+	# nvim -c "CocInstall coc-elixir"
 }
 
-post () {
-    term_color_red
-    echo "Done"
-    term_color_white
+post() {
+	term_color_red
+	echo "Done"
+	term_color_white
 }
 
 trap term_color_white EXIT
