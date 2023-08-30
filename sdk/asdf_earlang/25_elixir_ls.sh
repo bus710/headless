b9426c073a3c5a4fce0f349a53613ebcb88cb0d7
bus710 <bus710@gmail.com>
2023-08-30 13:36:28 -0700

    progress

--- sdk/asdf_earlang/25_elixir_ls.sh
+++ sdk/asdf_earlang/25_elixir_ls.sh
@@ -16,20 +16,21 @@ term_color_white() {
 }
 
 install_language_server() {
+
 	term_color_red
 	echo "install language server for nvim"
 	term_color_white
 
-	# term_color_red
-	# echo "install elixir-ls for nvim coc"
-	# term_color_white
-	#
-	# cd ~/Downloads
-	# LS_VERSION=$(curl -o- -s https://api.github.com/repos/elixir-lsp/elixir-ls/releases/latest | jq -r '.tag_name')
-	# wget https://github.com/elixir-lsp/elixir-ls/releases/download/${LS_VERSION}/elixir-ls-${LS_VERSION}.zip \
-	#     -O elixir-ls.zip
-	# unzip elixir-ls.zip -d /home/$LOGNAME/.config/nvim/plugged/coc-elixir/els-release
-	# rm -rf /home/$LOGNAME/Downloads/elixir-ls.zip
+	term_color_red
+	echo "install elixir-ls for nvim coc"
+	term_color_white
+
+	cd ~/Downloads
+	LS_VERSION=$(curl -o- -s https://api.github.com/repos/elixir-lsp/elixir-ls/releases/latest | jq -r '.tag_name')
+	wget https://github.com/elixir-lsp/elixir-ls/releases/download/${LS_VERSION}/elixir-ls-${LS_VERSION}.zip \
+		-O elixir-ls.zip
+	unzip elixir-ls.zip -d /home/$LOGNAME/.config/nvim/plugged/coc-elixir/els-release
+	rm -rf /home/$LOGNAME/Downloads/elixir-ls.zip
 
 	term_color_red
 	echo "install and build elixir-ls"
@@ -53,6 +54,12 @@ post() {
 	term_color_white
 }
 
+norun() {
+	echo "elixir-tools nvim plugin installs elixir-ls by itself"
+	exit
+}
+
 trap term_color_white EXIT
-install_language_server
-post
+norun
+#install_language_server
+#post
