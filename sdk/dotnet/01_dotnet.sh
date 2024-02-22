73cf5fffee31dc52e0dd9b2e95281a54d6eea9ac
bus710 <bus710@gmail.com>
2024-02-21 20:09:10 -0800

    progress

---
+++ sdk/dotnet/01_dotnet.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+
+# https://learn.microsoft.com/en-us/dotnet/core/install/linux-debian
+# https://github.com/AstroNvim/astrocommunity/blob/main/lua/astrocommunity/pack/cs/README.md
+# https://github.com/razzmatazz/csharp-language-server
+# https://aaronbos.dev/posts/csharp-dotnet-neovim
