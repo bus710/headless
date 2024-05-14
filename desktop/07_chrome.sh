6ab13bd443d06fd57f16ce35cf76056870d455bc
bus710 <bus710@gmail.com>
2024-05-13 18:04:11 -0700

    progress

--- desktop/07_chrome.sh
+++ desktop/07_chrome.sh
@@ -12,21 +12,29 @@ if [[ $XDG_CURRENT_DESKTOP =~ "ubuntu:GNOME" ]]; then
         chrome-gnome-shell
 fi
 
-echo 
-echo "Install Chrome Browser"
-echo 
+function install(){
+    echo 
+    echo "Install Chrome Browser"
+    echo 
 
-URL="https://dl.google.com/linux/direct/"
-PACKAGE_NAME="google-chrome-stable_current_amd64.deb"
+    URL="https://dl.google.com/linux/direct/"
+    PACKAGE_NAME="google-chrome-stable_current_amd64.deb"
 
-cd ~/Downloads
-wget ${URL}${PACKAGE_NAME}
-sudo dpkg -i ${PACKAGE_NAME}
-rm ${PACKAGE_NAME}
+    cd ~/Downloads
+    wget ${URL}${PACKAGE_NAME}
+    sudo dpkg -i ${PACKAGE_NAME}
+    rm ${PACKAGE_NAME}
+}
 
-echo
-echo "Done"
-echo "- Enable chrome://flags/#enable-webrtc-pipewire-capturer for Google Meet screen sharing in SwayWM"
-echo "- Enable chrome://flags/#ozone-platform-hint as wayland for Google Meet screen sharing in SwayWM"
-echo "- Run sudo apt --fix-broken intall"
-echo 
+function post(){
+    echo
+    echo "Done"
+    echo "- Enable chrome://flags/#enable-webrtc-pipewire-capturer for Google Meet screen sharing in SwayWM"
+    echo "- Enable chrome://flags/#ozone-platform-hint as wayland for Google Meet screen sharing in SwayWM"
+    echo "- Run sudo apt --fix-broken intall"
+    echo
+}
+
+trap post EXIT
+install
+post
