190c1453bc365157df73da96a02715b0a523af6e
bus710 <bus710@gmail.com>
2025-04-18 18:18:55 -0700

    progress

--- 90_get_udev_rule.sh
+++ 90_get_udev_rule.sh
@@ -9,16 +9,33 @@ term_color_white () {
 }
 
 # Check the mac address of the card
+ENO=$(ls /sys/class/net | grep eno)
+
+if [[ $(cat $ENO | wc -m) =~ 0 ]]; then
+    ENO_MAC=$(cat /sys/class/net/$ENO/address)
+    ENO_NAME="eth0"
+
+    term_color_red
+    echo "Use the command below to set the udev rule for eth0:"
+    term_color_white
+
+    echo "sudo tee /etc/udev/rules.d/70-persistent-net.rules <<EOF"
+    echo "SUBSYSTEM==\"net\", ACTION==\"add\", DRIVERS==\"?*\", ATTR{address}==\"$ENO_MAC\", ATTR{dev_id}==\"0x0\", ATTR{type}==\"1\", KERNEL==\"eth*\", NAME=\"$ENO_NAME\""
+    echo "EOF"
+fi
+
 WLP=$(ls /sys/class/net | grep wl)
-MAC=$(cat /sys/class/net/$WLP/address)
-NAME="wlan0"
 
-term_color_red
-echo "Use the command below to set the udev rule:"
-term_color_white
+if [[ $(echo $WLP | wc -m) =~ 0 ]]; then
+    WLP_MAC=$(cat /sys/class/net/$WLP/address)
+    WLP_NAME="wlan0"
 
-echo "sudo tee /etc/udev/rules.d/70-persistent-net.rules <<EOF"
-echo "SUBSYSTEM==\"net\", ACTION==\"add\", DRIVERS==\"?*\", ATTR{address}==\"$MAC\", ATTR{dev_id}==\"0x0\", ATTR{type}==\"1\", KERNEL==\"wlan*\", NAME=\"$NAME\""
-echo "EOF"
+    term_color_red
+    echo "Use the command below to set the udev rule for wlan0:"
+    term_color_white
 
+    echo "sudo tee /etc/udev/rules.d/70-persistent-net.rules <<EOF"
+    echo "SUBSYSTEM==\"net\", ACTION==\"add\", DRIVERS==\"?*\", ATTR{address}==\"$WLP_MAC\", ATTR{dev_id}==\"0x0\", ATTR{type}==\"1\", KERNEL==\"wlan*\", NAME=\"$WLP_NAME\""
+    echo "EOF"
+fi
 echo
