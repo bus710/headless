ac73ac7df59c976fa2b9d0249d5a7ecea5adf813
bus710 <bus710@gmail.com>
2026-03-17 21:30:42 -0700

    progress

--- infra/36_k8s.sh
+++ infra/36_k8s.sh
@@ -100,9 +100,7 @@ install_containerd(){
     sudo chmod a+r /etc/apt/keyrings/docker.gpg
 
     echo \
-    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] \
-    https://download.docker.com/linux/debian \
-    $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+        "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
     sudo tee /etc/apt/sources.list.d/docker.list
 
     sudo apt-get update
