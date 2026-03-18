40c83385334d5660c7bb29d6a4de0023a6b2912e
bus710 <bus710@gmail.com>
2026-03-17 21:33:09 -0700

    progress

--- infra/36_k8s.sh
+++ infra/36_k8s.sh
@@ -94,10 +94,13 @@ install_containerd(){
     apt-get update -y
     apt-get install -y ca-certificates curl gnupg
 
+    sleep 1
+
     install -m 0755 -d /etc/apt/keyrings
-    curl -fsSL https://download.docker.com/linux/debian/gpg | \
-        sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+    curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
     sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+    sleep 1
 
     echo \
         "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
