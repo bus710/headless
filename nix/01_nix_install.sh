ca0d7927a3e44e6a69698749c617615c924f1e62
bus710 <bus710@gmail.com>
2026-07-23 18:54:36 -0700

    progress

--- nix/01_nix_install.sh
+++ nix/01_nix_install.sh
@@ -7,6 +7,6 @@ curl --proto '=https' --tlsv1.2 -L https://nixos.org/nix/install | sh -s -- --da
 echo ""
 echo "Source the nix-sh"
 
-. /nix/var/nix/profiles/default/etc/profile.d/nix-sh
+. /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
 
 echo "Done"
