4dda583b20bbf35ff4bba6bd88501b07b3432a4d
bus710 <bus710@gmail.com>
2026-07-26 22:12:01 -0700

    progress

--- nixos/01_hm.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-nix-channel --add https://github.com/nix-community/home-manager/archive/release-25.11.tar.gz home-manager
-nix-channel --update
-
-nix-shell '<home-manager>' -A install
--- nixos/02_nv.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-git clone --depth 1 \
-    https://github.com/bus710/astronvim-template \
-    /home/$LOGNAME/.config/nvim
